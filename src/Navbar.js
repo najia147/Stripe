62681c3bbc46db05739cca7d552e25ec90023514
najia <anajia231@gmail.com>
2024-09-19 08:25:54 +0430

    Stripe Submenu

--- src/Navbar.js
+++ src/Navbar.js
@@ -5,6 +5,10 @@ import { useGlobalContext } from './context'
 
 const Navbar = () => {
   const {openSidebar, openSubmenu, closeSubmenu} = useGlobalContext();
+  const displaySubmenu = (e) => {
+    console.log('hello world');
+    openSubmenu()
+  }
   return <nav>
     <div className='nav-center'>
       <div className='nav-header'>
@@ -15,17 +19,17 @@ const Navbar = () => {
       </div>
       <ul className='nav-links'>
         <li>
-          <button className='link-btn'>
+          <button className='link-btn' onMouseOver={displaySubmenu}>
             products
           </button>
         </li>
         <li>
-          <button className='link-btn'>
+          <button className='link-btn' onMouseOver={displaySubmenu}>
             developers
           </button>
         </li>
         <li>
-          <button className='link-btn'>
+          <button className='link-btn' onMouseOver={displaySubmenu}>
             company
           </button>
         </li>
